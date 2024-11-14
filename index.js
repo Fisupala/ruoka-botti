f50e35cd4e0723fb18f908eb14d50b9bde47c055
Lauri <lauriilmari88@gmail.com>
2024-11-14 19:57:50 +0200

    I have again no idea what I did

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ async function hae_kaunis_ruokalista() {
   const foodlist = await getFoodList();
   //console.log(foodlist);
   const foodlistParsed = parser.parse(foodlist);
-  //console.log(foodlistParsed);
+  console.log(foodlistParsed.rss.channel);
   //console.log(foodlistParsed.rss.channel.item);
   //console.log(foodlistParsed.rss.channel.item.description);
-  return foodlistParsed.rss.channel.item.description;
+  return foodlistParsed.rss.channel.item;
 }
 
 //await console.log(await hae_kaunis_ruokalista());
@@ -28,7 +28,7 @@ function foo() {
 
 async function getFoodList() {
   let food;
-  await fetch("https://aromimenu.cgisaas.fi/EspooAromieMenus/FI/Default/ESPOO/Lintumetsankoulu/Rss.aspx?Id=221d8290-97ee-442f-b604-11b0f6dd2346&amp;DateMode=0").then(vastaus => {
+  await fetch("https://aromimenu.cgisaas.fi/EspooAromieMenus/FI/Default/ESPOO/Lintumetsankoulu/Rss.aspx?Id=bf4e16af-ddcc-4e46-9733-b424f19e3939&DateMode=1").then(vastaus => {
     food = vastaus.text()
   });
 
@@ -39,8 +39,8 @@ async function getFoodList() {
 async function embedRakentaja() {
   let ruokalista = await hae_kaunis_ruokalista();
 
-//  console.log(ruokalista.split("<br><br>"));
-  let ruokalistat = ruokalista.split("<br><br>")
+  //  console.log(ruokalista.split("<br><br>"));
+  //  let ruokalistat = ruokalista.split("<br><br>")
   const exampleEmbed = {
     color: 0xbe9130,
     title: 'Ruokalista',
@@ -60,24 +60,44 @@ async function embedRakentaja() {
     }
   };
 
-  for (let osa in ruokalistat) {
-    //console.log(osa);
+  for (let osa in ruokalista) {
+    console.log(osa);
 
-    let otsikko = ruokalistat[osa].split(":");
+    //let otsikko = ruokalistat[osa].split(":");
     //console.log(otsikko);
     exampleEmbed.fields.push({
-      name: otsikko[0],
-      value: otsikko[1],
+      name: ruokalista[osa].title,
+      value: "",
       inline: false,
     });
-  }
+    let paiva = ruokalista[osa].description.split("<br><br>")
 
+    let Kasvislounas = paiva[0].split(":")
+    exampleEmbed.fields.push({
+      name: "Kasvislounas",
+      value: Kasvislounas[1],
+      inline: true,
+    });
+
+    let Normaalilounas = paiva[1].split(":")
+    exampleEmbed.fields.push({
+      name: "Normaalilounas",
+      value: Normaalilounas[1],
+      inline: true,
+    });
+    exampleEmbed.fields.push({
+      name: "\u200B",
+      value: "",
+      inline: false,
+    });
+
+  }
   return exampleEmbed;
 }
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
-  
+
   if (interaction.commandName === 'anna_ruokaa') {
     await interaction.reply({ embeds: [await embedRakentaja()] });
   }
@@ -86,12 +106,11 @@ client.on('interactionCreate', async interaction => {
 /*
 client.on('interactionCreate', async interaction => {
   if (!interaction.isChatInputCommand()) return;
-
+ 
   if (interaction.commandName === 'anna_ruokaa') {
-
+ 
     interaction.reply(await hae_kaunis_ruokalista());
   }
 });
 */
 client.login(process.env.BOT_TOKEN);
-
